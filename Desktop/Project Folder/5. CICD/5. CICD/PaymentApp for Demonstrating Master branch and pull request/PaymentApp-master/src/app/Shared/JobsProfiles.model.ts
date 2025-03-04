a47102aa896b392446dcbd6e4e8b54847ca1c0df
yvonne234 <remwan@yahoo.com>
2025-03-04 05:55:00 -0500

    Remove ignored files from repository

--- Desktop/Project Folder/5. CICD/5. CICD/PaymentApp for Demonstrating Master branch and pull request/PaymentApp-master/src/app/Shared/JobsProfiles.model.ts
+++
@@ -1,3 +0,0 @@
-export class JobsProfiles{
-    constructor(public id: number, public jobTitle?: string, public skills?: string){}
-}
--- Desktop/Project Folder/5. CICD/5. CICD/PaymentApp for Demonstrating Master branch and pull request/PaymentApp-master/src/app/Shared/card.model.ts
+++
@@ -1,3 +0,0 @@
-export class Card{
-    constructor(public type: string){}
-}
